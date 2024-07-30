6c50593ecad4783684da36ead69c4569684e5e85
Vitalii <tather13@gmail.com>
2024-07-30 10:22:21 +0300

    propsPractice

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,25 +1,33 @@
 import AppInfo from '../app-info/app-info';
 import SearchPanel from '../search-panel/search-panel';
 import AppFilter from '../app-filter/app-filter';
-import EmployeesList from '../employees-list/employees-list';
-import EmployeesAddForm from '../employees-add-form/employees-add-form';
+import EmployersList from '../employers-list/employers-list';
+import EmployeesAddForm from '../employers-add-form/employers-add-form';
 
 import './app.css';
 
 function App() {
-  return (
-    <div className="app">
-        <AppInfo />
 
-        <div className="search-panel">
-            <SearchPanel/>
-            <AppFilter/>
+    const data = [
+        { name: 'John C.', salary: 800, increase: true },
+        { name: 'Alex M.', salary: 3000, increase: true },
+        { name: 'Carl W.', salary: 5000, increase: true }
+    ];
+
+    return (
+        <div className="app">
+            <AppInfo />
+
+
+            <div className="search-panel">
+                <SearchPanel />
+                <AppFilter />
+            </div>
+
+            <EmployersList data={data} />
+            <EmployeesAddForm />
         </div>
-        
-        <EmployeesList/>
-        <EmployeesAddForm/>
-    </div>
-  );
+    );
 }
 
 export default App;
--- src/components/search-panel/search-panel.js
+++ src/components/search-panel/search-panel.js
@@ -1,10 +1,10 @@
-import './search-panel.css';
+import './search-panel.css'
 
 const SearchPanel = () => {
     return (
         <input type="text"
-                className="form-control search-input"
-                placeholder="Найти сотрудника"/>
+            className="form-control search-input"
+            placeholder="Найти сотрудника" />
     )
 }
 
