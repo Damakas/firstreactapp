eb151daaa1a8c9e3ec665b07d91b0c54f7e2e99d
Vitalii <tather13@gmail.com>
2024-07-30 11:46:42 +0300

    componentsStateWithClass

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -3,15 +3,43 @@ import SearchPanel from '../search-panel/search-panel';
 import AppFilter from '../app-filter/app-filter';
 import EmployersList from '../employers-list/employers-list';
 import EmployeesAddForm from '../employers-add-form/employers-add-form';
+import { Component } from 'react'
 
 import './app.css';
+
+class WhoAmI extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            years: 27,
+            text: '+++'
+        }
+    }
+
+    nextYear = () => {
+        this.setState(state => ({
+            years: state.years + 1
+        }))
+    }
+
+    render() {
+        const { name, surname, link } = this.props;
+        return (
+            <div>
+                <button onClick={this.nextYear} >{this.state.text}</button>
+                <h1>My name is {name}, surname - {surname} age -{this.state.years}</h1>
+                <a href={link}>My profile</a>
+            </div>
+        )
+    }
+}
 
 function App() {
 
     const data = [
-        { name: 'John C.', salary: 800, increase: true },
-        { name: 'Alex M.', salary: 3000, increase: true },
-        { name: 'Carl W.', salary: 5000, increase: true }
+        { name: 'John C.', salary: 800, increase: false, id: 1 },
+        { name: 'Alex M.', salary: 3000, increase: true, id: 2 },
+        { name: 'Carl W.', salary: 5000, increase: false, id: 3 }
     ];
 
     return (
@@ -26,7 +54,14 @@ function App() {
 
             <EmployersList data={data} />
             <EmployeesAddForm />
+
+            <div className='test'>
+                <WhoAmI name='John' surname='Smith' link='facebook.com' />
+                <WhoAmI name='Alex' surname='Shepard' link='vk.com' />
+            </div>
         </div>
+
+
     );
 }
 
